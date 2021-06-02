fb9fdefb2b244e0f77e5d7be666469a056b06438
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-06-02 09:50:26 +0530

    [Prajakta] Added: User Repository

---
+++ src/main/java/com/bridgelabz/imp/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.imp.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.bridgelabz.imp.model.UserData;
+
+//@Repository
+public interface UserRepository extends JpaRepository<UserData, Long> 
+{
+
+
+
+	 Optional<UserData> findByFullname(String fullname);
+
+    public Optional<UserData> findById(String id);
+
+	
+
+}
