dab866191b5f06182723c1de8d2aea8228c694e6
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-06-02 09:47:55 +0530

    [Prajakta] Added: User DTO for validation

---
+++ src/main/java/com/bridgelabz/imp/dto/UserDTO.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.imp.dto;
+import java.util.List;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import lombok.Data;
+
+@Data
+public class UserDTO 
+{
+	@Pattern(regexp =  "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Invalid name")
+	private String fullname;
+		
+	@NotEmpty(message = "Mobile number cannot be null")
+	private String mobilenumber;
+	
+	@NotEmpty(message = "Address cannot be null")	
+	private List<String> address;
+	
+	@NotEmpty(message = "Vehicledata cannot be null")	
+	private String vehicledata;
+	
+	@NotNull(message = "Age cannot be null")
+	private int age;
+	
+	@NotEmpty(message = "Occupation cannot be null")	
+	private String occupation;
+	
+	@NotEmpty(message = "HealthCondition cannot be null")	
+	private String healthcondition;
+	
+	@NotEmpty(message = "familyBackground cannot be null")	
+	private String familybackground;
+	
+	@NotEmpty(message = "kyc cannot be null")	
+	private String kyc;
+	
+	
+}
+
